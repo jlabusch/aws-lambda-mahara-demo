c2912dc49e9d9ea44f0b8cd8cee1905ce8d73a3b
Jacques Labuschagne <jlabusch@acm.org>
2016-08-24 00:34:57 +0100

    Working on lambda-1

---
+++ lambda-1-fetch/index.js
@@ -0,0 +1,37 @@
+var config = require('./config.json');
+
+exports.handler = function(event, context, next){
+    var ftp = require('ftp');
+
+    var client = new ftp();
+
+    client.on('ready', function(){
+        client.list(function(err, list){
+            if (err){
+                next && next(err);
+                return;
+            }
+            client.get('mahara_users.csv', function(err, stream){
+                if (err){
+                    next && next(err);
+                    return;
+                }
+                var buffer = '';
+                stream.on('data', function(chunk){
+                    buffer = buffer + chunk.toString();
+                });
+                stream.on('error', function(err){
+                    next && next(err);
+                });
+                stream.once('close', function(){
+                    client.end();
+                    console.error('Got buffer with ' + buffer.length + ' bytes');
+                    next && next(null, buffer);
+                });
+            });
+        });
+    });
+
+    client.connect(config);
+}
+
